69fd8614c0b443b532ddcda87c5ab88e7dba367d
Lima <linam.errico@gmail.com>
2023-07-16 10:48:18 -0400

    js updated and link in HTML fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,19 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+/*WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria:
+
+1.  WHEN prompted for the length of the password
+    THEN I choose a length of at least 8 characters and no more than 128 characters*/
+function generatePassword() {
+  var pswdlength = parseInt(prompt("Enter the length of the password of at least 8 and no more than 128 characters:"));
+  
+  if(pswdlength < 8 || pswdlength > 128 || isNaN(pswdlength)) {
+    alert("Invalid length");
+  } 
+
+}
+    
+
